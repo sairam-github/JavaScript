bbba587507dde3619c837071f2bb5cc0302075f8
Sairam Madem <smadem@ptc.com>
2019-08-02 09:04:16 -0400

    String concatenate operations.

---
+++ stringconcatenate.js
@@ -0,0 +1,10 @@
+var myStr = "This is my first string. " + "\n" + "This is my second string.";
+myStr += "\n" + "This is my third string.";
+
+console.log(myStr);
+
+var anAdjective = "awesome!";
+var outStr = "Learning code is ";
+myStr = outStr + anAdjective;
+
+console.log(myStr);
