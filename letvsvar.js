fa1fefc3d7fd34bc613613924449ff0806ec3c87
Sairam Madem <smadem@ptc.com>
2019-08-02 14:02:34 -0400

    Difference between let and var.

---
+++ letvsvar.js
@@ -0,0 +1,15 @@
+// Accessing 'var' after definition
+var x = 5;
+console.log(x);
+
+// Accessing 'let' after definition
+let y = 10;
+console.log(y);
+
+// Accessing 'var' before declaration/definition
+console.log(z);
+var z;
+
+// Accessing 'let' before definition
+console.log(a);
+let a = 20;
