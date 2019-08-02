896b00a9ce55481eb40561d1ac02a3229d29021f
Sairam Madem <smadem@ptc.com>
2019-08-02 09:25:50 -0400

    More string operations

---
+++ stringops.js
@@ -0,0 +1,5 @@
+var myStr = "Learning code is awesome!";
+console.log(myStr.length);
+console.log(myStr[0]);
+console.log(myStr.split(" "));
+console.log(myStr.substring(5,10)); // 10th character isn't included in output
