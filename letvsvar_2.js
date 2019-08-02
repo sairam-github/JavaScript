441083f37cb9bba431532830466ed592aab98d42
Sairam Madem <smadem@ptc.com>
2019-08-02 14:09:45 -0400

    More on let vs var.

---
+++ letvsvar_2.js
@@ -0,0 +1,8 @@
+// let won't let us use same name twice for declaration
+let xValue = 10;
+let xValue = 20;
+
+// we can use same name for declaration more than once
+var yValue = 10;
+var yValue = 20;
+
