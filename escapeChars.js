077d63fdc87c2346726ada135051aab5c4863646
Sairam Madem <smadem@ptc.com>
2019-08-02 08:56:48 -0400

    Escape characters

---
+++ escapeChars.js
@@ -0,0 +1,14 @@
+/***
+ \'             single quote
+ \"            double quote
+ \\             backlash
+ \n             new line
+ \r             carriage return
+ \t             tab
+ \b             backspace
+ \f             form feed          
+ 
+ ***/
+
+ var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"
+ console.log(myStr);
