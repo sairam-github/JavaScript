cca9d80042069bc2d4856f45ed0e0bd7744c852c
Sairam Madem <smadem@ptc.com>
2019-08-02 09:36:35 -0400

    Array operations

---
+++ arrayops.js
@@ -0,0 +1,17 @@
+var myArray = [50, 60, 70];
+console.log(myArray[0]);
+
+var myMultiArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log(myMultiArray[0][2]);
+
+myArray.push(80);
+console.log(myArray);
+
+myArray.pop();
+console.log(myArray);
+
+myArray.shift();
+console.log(myArray);
+
+myArray.unshift(50);
+console.log(myArray);
